44d7cf904bdc3253b9cc48873cc2069b47bad6de
Kieran Bacon <Kieran.Bacon@outlook.com>
2017-11-16 16:25:47 +0000

    Changes to the database schema

--- Webtool/ExpertWebtool/data/ConstructDatabase.py
+++ Webtool/ExpertWebtool/data/ConstructDatabase.py
@@ -12,7 +12,7 @@ for table in tables:
 
 # Create tables in the database
 
-cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, salt TEXT, password TEXT, firstname TEXT, lastname TEXT)")
+cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, salt TEXT, password TEXT, firstname TEXT, lastname TEXT, avatar TEXT)")
 cursor.execute("CREATE TABLE models (mid PRIMARY KEY, filepath TEXT)")
 cursor.execute("CREATE TABLE questions (qid PRIMARY KEY, text TEXT)")
 cursor.execute("CREATE TABLE labels (username TEXT, mid INT, qid INT, score INT)")
@@ -21,22 +21,22 @@ cursor.execute("CREATE TABLE labels (username TEXT, mid INT, qid INT, score INT)
 #	- Kieran's
 salt = uuid.uuid4().hex
 hashedPassword = hashlib.sha512((salt + "admin1").encode("UTF-8")).hexdigest()
-cursor.execute("INSERT INTO users VALUES ('kb437', ?, ?, 'Kieran', 'Bacon')", (salt, hashedPassword))
+cursor.execute("INSERT INTO users VALUES ('kb437', ?, ?, 'Kieran', 'Bacon', 'imgs/avatars/avatar-ninja.png')", (salt, hashedPassword))
 
 #	- Ben's
 salt = uuid.uuid4().hex
 hashedPassword = hashlib.sha512((salt + "password").encode("UTF-8")).hexdigest()
-cursor.execute("INSERT INTO users VALUES ('ben', ?, ?, 'Ben', 'Townsend')", (salt, hashedPassword))
+cursor.execute("INSERT INTO users VALUES ('ben', ?, ?, 'Ben', 'Townsend', 'imgs/avatars/avatar-professional-m.png')", (salt, hashedPassword))
 
 #	- Paul's
 salt = uuid.uuid4().hex
 hashedPassword = hashlib.sha512((salt + "password").encode("UTF-8")).hexdigest()
-cursor.execute("INSERT INTO users VALUES ('paul', ?, ?, 'Paul', 'Kim')", (salt, hashedPassword))
+cursor.execute("INSERT INTO users VALUES ('paul', ?, ?, 'Paul', 'Kim', 'imgs/avatars/avatar-professional-f.png')", (salt, hashedPassword))
 
 #	- Nick's
 salt = uuid.uuid4().hex
 hashedPassword = hashlib.sha512((salt + "password").encode("UTF-8")).hexdigest()
-cursor.execute("INSERT INTO users VALUES ('nick', ?, ?, 'Nick', 'Higgins')", (salt, hashedPassword))
+cursor.execute("INSERT INTO users VALUES ('nick', ?, ?, 'Nick', 'Higgins', 'imgs/avatars/avatar-professional-f.png')", (salt, hashedPassword))
 
 # Commit the changes close the connection to the database
 database.commit()
