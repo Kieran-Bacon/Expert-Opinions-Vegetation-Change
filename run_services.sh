3a4bb179128d5acce27a95d0bb68571d6871177c
benleetownsend <ben@liskeard.me.uk>
2018-02-06 15:20:56 +0000

    Added persistant storage to containers

--- run_services.sh
+++ run_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-docker run -p 6543:6543 group_proj_main bash /root/code/docker_run_on_start.sh&
+docker run -p 6543:6543 -v group_proj_storage:/root/code group_proj_main bash /root/code/docker_run_on_start.sh&
