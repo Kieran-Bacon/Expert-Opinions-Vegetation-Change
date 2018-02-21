3f01688ba74dafcbd3004892186ff48c1fccf66a
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-02-21 21:16:11 +0000

    E-mail functionality moved
    A new function within helper has been set up to allow emailing from a consistent location

--- Server/ExpertWebtool/Helper.py
+++ Server/ExpertWebtool/Helper.py
@@ -1,12 +1,39 @@
 import os, uuid
 import re
 
+import smtplib
+from email.message import EmailMessage
+
 import pyramid.httpexceptions as exc
 
-from . import TEMPSTORAGE
+from . import TEMPSTORAGE, TEMPLATES
 
 # Regular expressions for cross site things
 EMAIL_REGEX = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+def email(subject: str, to: str, template: str, formatting: [str]):
+
+	# Create email information
+	email = EmailMessage()
+	email["Subject"] = subject
+	email["To"] = to
+	email["From"] = "ExpertClimateWebtool@gmail.com"
+
+	# Set e-mail contents
+	with open(os.path.join(TEMPLATES, template)) as filehandler:
+		email.set_content(filehandler.read().format(*formatting))
+
+	# Send the message via our own SMTP server.
+	s = smtplib.SMTP(host='smtp.gmail.com', port=587)  
+	s.starttls()
+	s.login("ExpertClimateWebtool@gmail.com", "Exeter1!")
+	s.send_message(email)
+	s.quit()
+
+class Warehouse:
+	# TODO :: Make this a thing 
+	vault = {}
+	pass
 
 class HiddenPages:
 
