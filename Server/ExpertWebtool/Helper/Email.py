f1ece0e392b4582298bfed306200fa6ecba03497
Kieran Bacon <Kieran.Bacon@outlook.com>
2018-04-20 02:02:57 +0100

    Expanded the helper module for clearer use
    The helper module contained usefull classes and objects that were used all across the website
    These are not expanded into their own modules with the Helper folder, allows for easier development in the future.

---
+++ Server/ExpertWebtool/Helper/Email.py
@@ -0,0 +1,23 @@
+from ExpertWebtool import TEMPLATES
+
+import smtplib
+from email.message import EmailMessage
+
+def email(subject: str, to: str, template: str, formatting: [str]):
+
+	# Create email information
+	email = EmailMessage()
+	email["Subject"] = subject
+	email["To"] = to
+	email["From"] = "ExpertClimateWebtool@gmail.com"
+
+	# Set e-mail contents
+	with open(os.path.join(TEMPLATES, template)) as filehandler:
+		email.set_content(filehandler.read().format(*formatting))
+
+	# Send the message via our own SMTP server.
+	s = smtplib.SMTP(host='smtp.gmail.com', port=587)  
+	s.starttls()
+	s.login("ExpertClimateWebtool@gmail.com", "Exeter1!")
+	s.send_message(email)
+	s.quit()
---
+++ Server/ExpertWebtool/Helper/__init__.py
@@ -0,0 +1,51 @@
+from .Email import email
+from .Storage import CMOStore, tempStorage, emptyDirectory
+from .HiddenPages import HiddenPages
+from .Permissions import permissions
+
+import re, uuid, hashlib
+import pyramid.httpexceptions as exc
+
+from ExpertWebtool.DatabaseHandler import DatabaseHandler as db
+
+# Regular expressions for cross site things
+EMAIL_REGEX = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+class Warehouse:
+	# TODO :: Make this a thing 
+	vault = {}
+	pass
+
+def pageVariables(request, additional=None) -> dict:
+
+	alerts = request.session.get("alerts", [])
+	request.session["alerts"] = []
+
+	if additional is None:
+		return {**request.session, "alerts":alerts}
+	return {**request.session, **additional, "alerts":alerts}
+
+def hashPassword(pwd):
+	salt = uuid.uuid4().hex
+	hashedPassword = hashlib.sha512((salt + pwd).encode("UTF-8")).hexdigest()
+	return salt, hashedPassword
+
+def recordModelMetrics(identifier: str, metrics = None) -> None:
+	""" Handle the model output that comes from the expert repr """
+
+	try:
+		precision = metrics.precision
+	except:
+		precision = None
+
+	try:
+		accuracy = metrics.accuracy
+	except:
+		accuracy = None
+
+	try:
+		R2 = metrics.R2
+	except:
+		R2 = None
+
+	db.execute("eModel_recordMetrics", [precision, accuracy, R2, identifier]) # Record the model metrics
