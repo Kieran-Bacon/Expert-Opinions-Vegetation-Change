b5f0b01b0be4d4f350b89ff25fd5987e410e7568
Kieran Bacon <kieran.bacon@outlook.com>
2018-05-10 12:45:25 +0100

    build script

--- VegML.sh
+++ VegML.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-docker build . -t expertclimatesystem:latest
-docker run -p 80:6543 -v group_proj_storage_data:/root/code/Server/ExpertWebtool/data expertclimatesystem
+name="expertclimatesystem"
+port=80
+volume="group_proj_storage_data"
+
+while [ $# -ge 2 ]
+    do
+        if [ "$1" = "-t" ]
+            then 
+                name=$2
+        fi
+
+        if [ "$1" = "-p" ]
+            then
+                port=$2
+        fi
+
+        if [ "$1" = "-v" ]
+            then
+                volume=$2
+        fi
+    shift
+    shift
+done
+
+echo $name
+echo $port 
+echo $volume
+
+docker build . -t "$name":latest
+docker run -p "$port":6543 -v \"$volume\":/root/code/Server/ExpertWebtool/data "$name"
