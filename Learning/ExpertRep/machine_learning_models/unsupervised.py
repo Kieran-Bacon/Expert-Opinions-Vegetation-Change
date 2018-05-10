b439b1a591d779efea94013b2ca51a1a066666dd
benleetownsend <ben@liskeard.me.uk>
2018-05-10 09:44:32 +0100

    Added testing models and updated ubuntu container version

--- Learning/ExpertRep/machine_learning_models/unsupervised.py
+++ Learning/ExpertRep/machine_learning_models/unsupervised.py
@@ -4,6 +4,7 @@ sklearn PCA implementation.
 """
 import copy
 import numpy as np
+import pickle
 
 from sklearn.decomposition import PCA as SK_PCA
 
@@ -17,7 +18,7 @@ class PCA(UnsupervisedBase):
 
     def __init__(self):
         super().__init__()
-        self.pca = SK_PCA(n_components=10)
+        self.pca = SK_PCA(n_components=0.95)
 
     def fit(self, data: list) -> None:
         """
@@ -25,6 +26,40 @@ class PCA(UnsupervisedBase):
         """
         data_array = [np.reshape(d.get_numpy_arrays(), [-1]) for d in data]
         self.pca.fit(data_array)
+
+    def predict(self, data: list) -> list:
+        """
+        See UnsupervisedBase for params
+        """
+        data_array = [np.reshape(d.get_numpy_arrays(), [-1]) for d in data]
+        projected_data = copy.deepcopy(data)
+        for i, dat in enumerate(np.split(self.pca.transform(data_array), len(data))):
+            projected_data[i].numpy_arrays = dat
+        return projected_data
+
+class PCANPickle(UnsupervisedBase):
+    """
+    A simple adaptor for SKlearn PCA to the ExpertRep API.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.pca = SK_PCA(n_components=99.9)
+        self.saved = False
+
+    def fit(self, data: list) -> None:
+        """
+        See UnsupervisedBase for params
+        """
+        data_array = [np.reshape(d.get_numpy_arrays(), [-1]) for d in data]
+        self.pca.fit(data_array)
+        components = self.pca.components_
+        variance = self.pca.explained_variance_ratio_
+        if not self.saved:
+            self.saved = True
+            with open("PCA_DATA.pkl", "wb") as fp:
+                pickle.dump((components, variance), fp)
+
 
     def predict(self, data: list) -> list:
         """
